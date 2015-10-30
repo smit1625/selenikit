e4014a35c6724fa4644cecaf3c50eebfdab41bdf
MacKinley Smith <smit1625@msu.edu>
2015-10-29 19:40:33 -0600

    Converted to block syntax

--- lib/selenikit/rspec/helpers.rb
+++ lib/selenikit/rspec/helpers.rb
@@ -2,9 +2,9 @@ module Selenikit
   module Rspec
     module Helpers
       RSpec.configure do |config|
-        config.before(:each) do
+        config.before(:each) do |example|
           # If the test is tagged as selenium or if we're in the selenium rake task, set the driver to selenium
-          if self.example.metadata[:selenium].present? || ENV["SELENIUM"] == "true"
+          if example.metadata[:selenium].present? || ENV["SELENIUM"] == "true"
             Capybara.current_driver = :selenium_firefox_driver
           end
         end
